bbdfb5db1ddd8cc07e56bff09a74b60e37b41309
Conor McColgan <50048517@belfastmet.ac.uk>
2022-03-02 14:33:48 +0000

    First Commit

---
+++ css/javascript/app.js
@@ -0,0 +1,7 @@
+function changeTheme(){
+    let colours = ["red", "greeen", "blue"];
+    for(i=0; i < colours.length; i++){
+        //console.log(colours[i]);
+        document.body.style.backgroundColor = colours[i];
+    }
+}
